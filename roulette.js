a5a2184459592c96f180d9fb788715d204660756
Jordi Pun <jordipun@Jordis-MacBook-Pro.local>
2019-06-20 23:57:54 -0600

    done details

--- roulette.js
+++ roulette.js
@@ -5,6 +5,7 @@ function roulette(){
     let genreids = [];
     let genreurl = "https://api.themoviedb.org/3/genre/movie/list?api_key=8d5d0ae3269d0d64b7c94cbeb92c4de7&language=en-US";
     let posterurl = "http://image.tmdb.org/t/p/w500";
+    let allgenres = [];
 
 
 
@@ -18,6 +19,7 @@ function roulette(){
                     genreids.push(data["genres"][x]["id"])
 
                 }
+                allgenres.push(data["genres"][x])
 
             }
 
@@ -54,6 +56,7 @@ function roulette(){
                     document.body.style.color = 'black';
                     h.style.display = 'inline-block';
                     h.style.verticalAlign = 'middle';
+                    document.body.style.paddingTop= '100px';
 
                     h.appendChild(t);
                     document.body.appendChild(h);
@@ -65,6 +68,9 @@ function roulette(){
                     poster.style.marginRight = 'auto';
                     poster.style.width = '50';
                     document.body.appendChild(poster);
+
+                    tableCreate(movie,allgenres);
+
 
                 })
 
@@ -97,4 +103,70 @@ function discovermovie (genreids) {
             return json;
 
         })
+}
+
+function tableCreate(movie,allgenres){
+    let body = document.body,
+        tbl  = document.createElement('table');
+    tbl.style.width  = '100px';
+    tbl.style.border = '1px solid black';
+    let genrenames = [];
+
+
+    for(let x = 0; x<movie["genre_ids"].length;x++){
+        for(let y = 0; y <allgenres.length;y++){
+            if (movie["genre_ids"][x]==allgenres[y]["id"]){
+                genrenames.push(allgenres[y]["name"])
+            }
+        }
+
+
+    }
+
+    console.log(genrenames)
+
+    for(let i = 0; i < 4; i++){
+        let tr = tbl.insertRow();
+        for(let j = 0; j < 2; j++){
+
+            let td = tr.insertCell();
+            if (j == 1 ){
+                if (i == 0 ){
+                    td.appendChild(document.createTextNode(movie['release_date']))
+                }
+                else if(i == 1){
+                    td.appendChild(document.createTextNode(movie['overview']))
+                }
+                else if(i == 2){
+                    td.appendChild(document.createTextNode(genrenames.join()))
+                }
+                else if(i==3){
+                    td.appendChild(document.createTextNode(movie['vote_average']))
+                }
+            }
+            if (j == 0 ){
+                if (i == 0 ){
+                    td.appendChild(document.createTextNode("Release Date"))
+                }
+                else if(i == 1){
+                    td.appendChild(document.createTextNode("Overview"))
+                }
+                else if(i == 2){
+                    td.appendChild(document.createTextNode("Genres"))
+                }
+                else if(i==3){
+                    td.appendChild(document.createTextNode("Vote Average"))
+                }
+            }
+
+            td.style.border = '1px solid black';
+
+
+        }
+    }
+    tbl.align = "center";
+    tbl.style.width = "800px";
+    tbl.style.height = "400px";
+    tbl.style.paddingTop = "30px";
+    body.appendChild(tbl);
 }
